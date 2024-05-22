0e0053f460a1f450d2c9c5ced2fd2bec82c94f19
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-22 18:04:32 -0400

    Clearning the innerHTML on submit fixes the duplicate cards issue

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ let addToy = false;
 let toyArray = []
 
 document.addEventListener("DOMContentLoaded", (e) => {
+  e.preventDefault()
   const addBtn = document.querySelector("#new-toy-btn");
-  const toyFormContainer = document.querySelector(".container");
   const toyCollection = document.getElementById("toy-collection");
 
   addBtn.addEventListener("click", () => {
-    // hide & seek with the form
     addToy = !addToy;
     if (addToy) {
       toyFormContainer.style.display = "block";
@@ -16,65 +15,77 @@ document.addEventListener("DOMContentLoaded", (e) => {
     } 
   });
 
-  toyFormContainer.addEventListener("submit", formSubmit);
-  function formSubmit(e){
-    e.preventDefault()
-    let formData 
-    console.log(e.target.value)
-    fetch("http://localhost:3000/toys", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-      },
-      body: formData
-    })
-    .then(function(response) {
-      return response.json()
-    })
-    .then(function(data) {
-      console.log(data)
-      createCards()
-    })
+  const fetchToys = () =>{
+    fetch("http://localhost:3000/toys")
+      .then(function(response) {
+        return response.json()
+      })
+      .then(function(data) {
+        toyArray = data
+        createCards()
+      })
   }
-  
-  fetch("http://localhost:3000/toys")
-    .then(function(response) {
-      return response.json()
-    })
-    .then(function(data) {
-      toyArray = data
-      createCards()
-    })
 
-    const createCards = () => {
-      toyArray.forEach(function(toy) {
-        const div = document.createElement('div')
-        div.className = "card"
-        div.textContent = toy.id
-        
-        const h2 = document.createElement('h2')
-        h2.textContent = toy.name
-        
-        const img = document.createElement('img')
-        img.src = toy.image
-        img.classList.add("toy-avatar")
-        
-        const p = document.createElement('p')
-        p.textContent = (toy.likes)
+  const createCards = () => {
+    toyCollection.innerHTML = "";
+    toyArray.forEach(function(toy) {
+      const div = document.createElement('div')
+      div.className = "card"
+      div.textContent = toy.id
+      
+      const h2 = document.createElement('h2')
+      h2.textContent = toy.name
+      
+      const img = document.createElement('img')
+      img.src = toy.image
+      img.classList.add("toy-avatar")
+      
+      const p = document.createElement('p')
+      p.textContent = (toy.likes)
 
-        const button = document.createElement('button')
-        button.textContent = "Like ❤️"
-        button.className = "like-btn"
-        button.id = toy.id
-        
-        div.appendChild(h2)
-        div.appendChild(img)
-        div.appendChild(p)
-        div.appendChild(button)
+      const button = document.createElement('button')
+      button.textContent = "Like ❤️"
+      button.className = "like-btn"
+      button.id = toy.id
+      
+      div.appendChild(h2)
+      div.appendChild(img)
+      div.appendChild(p)
+      div.appendChild(button)
 
-        toyCollection.appendChild(div)
-      }) 
+      toyCollection.appendChild(div)
+    }) 
+  }
+
+  const toyFormContainer = document.querySelector(".container");
+  toyFormContainer.addEventListener("submit", (e) => {
+    e.preventDefault()
+    let toyNameInput = document.querySelectorAll(".input-text")[0]
+    let imgUrlInput = document.querySelectorAll(".input-text")[1] 
+
+    let toyName = toyNameInput.value;
+    let imgUrl = imgUrlInput.value;
+
+    const bodyData = {
+      name: toyName,
+      image: imgUrl,
+      likes: 0
     }
-
+      fetch("http://localhost:3000/toys", {
+        method: "POST",
+        body: JSON.stringify(bodyData),
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }
+      })
+        .then(function(response) {
+          return response.json()
+        })
+        .then(function(data) {
+          toyArray.push(data)
+          createCards();
+        })
+  })
+  fetchToys()
 })
