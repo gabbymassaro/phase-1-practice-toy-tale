12c03a0f9c2896a58e677f3ba5b3c6b20e96d3dd
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-22 10:29:20 -0400

    Create new div and classname using toy names

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 let addToy = false;
 let toyPicturesArray
-let toyImageUrls = []
+// let toyImageUrls = []
+
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
@@ -23,13 +24,28 @@ document.addEventListener("DOMContentLoaded", () => {
   })
   .then(function(data) {
     toyPicturesArray = data
-    for (const object of toyPicturesArray) {
-      let image = object.image;
-      toyImageUrls.push(image)
-    }
-  })
 
-  
+    // for (const object of toyPicturesArray) {
+    //   let image = object.image;
+    //   toyImageUrls.push(image)
+    // }
+
+    toyPicturesArray.forEach(function (toy) {
+      console.log(toy.name)
+      const div = document.createElement('div')
+      div.textContent = toy.name
+      toyCollection.appendChild(div).className = "cards"
+    })
+
+
+    // toyImageUrls.forEach(function (toy) {
+    //   const img = document.createElement('img')
+    //   img.src = toy
+    //   toyCollection.appendChild(img)
+    // })  
+
+
+  })
 
 });
 
