c0bc974511909f3eff858eae29a12344aaefb951
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-22 11:20:59 -0400

    Create all the toy cards and class

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 let addToy = false;
-let toyPicturesArray
-// let toyImageUrls = []
+let toyArray = []
 
 
 document.addEventListener("DOMContentLoaded", () => {
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   const toyCollection = document.getElementById("toy-collection")
+
 
   addBtn.addEventListener("click", () => {
     // hide & seek with the form
@@ -19,34 +19,38 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
   fetch("http://localhost:3000/toys")
-  .then(function(response) {
-    return response.json()
-  })
-  .then(function(data) {
-    toyPicturesArray = data
-
-    // for (const object of toyPicturesArray) {
-    //   let image = object.image;
-    //   toyImageUrls.push(image)
-    // }
-
-    toyPicturesArray.forEach(function (toy) {
-      console.log(toy.name)
-      const div = document.createElement('div')
-      div.textContent = toy.name
-      toyCollection.appendChild(div).className = "cards"
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(data) {
+      toyArray = data
+      createCards()
     })
 
+    const createCards = () => {
+      toyArray.forEach(function(toy) {
+        console.log(toy.id)
+        const div = document.createElement('div')
+        div.textContent = toy.id
+        toyCollection.appendChild(div).className = "cards"
 
-    // toyImageUrls.forEach(function (toy) {
-    //   const img = document.createElement('img')
-    //   img.src = toy
-    //   toyCollection.appendChild(img)
-    // })  
+        console.log(toy.name)
+        const h2 = document.createElement('h2')
+        h2.textContent = toy.name
+        toyCollection.appendChild(h2).className = "cards"
 
+        console.log(toy.iamge)
+        const img = document.createElement('img')
+        img.src = toy.image
+        toyCollection.appendChild(img).className = "cards"
 
-  })
-
+        console.log(toy.likes)
+        const p = document.createElement('p')
+        p.textContent = (toy.likes)
+        toyCollection.appendChild(p).className = "cards"
+      }) 
+    }
+    
 });
 
 
